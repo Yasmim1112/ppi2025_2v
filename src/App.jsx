d2b34f67ab26b61c7b9c118651f819943c709a1d
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-05-16 17:23:30 +0000

    Aula-16/05/25 - Configuração do css global e theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,13 @@
-import './App.css';
+import "./styles/theme.css";
+import "./styles/global.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+    <div>
+      <h1>Meu primeiro React App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, cumque ipsa aperiam earum est architecto voluptatibus obcaecati placeat sapiente impedit repellat eius ullam cum deserunt sint debitis praesentium eligendi saepe!
+      
+      </p>
     </div>
   );
 }
-
-export default App;
