c615e2f6a5df733eaf2c49523d1731c98b92c23c
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-05-22 17:19:56 +0000

    Aula-22-05-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import "./styles/theme.css";
 import "./styles/global.css";
+import { MyText } from "./components/MyText";
 
 export default function App() {
   return (
-    <div>
-      <h1>Meu primeiro React App</h1>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Molestiae, cumque ipsa aperiam earum est architecto voluptatibus obcaecati placeat sapiente impedit repellat eius ullam cum deserunt sint debitis praesentium eligendi saepe!
-      
-      </p>
-    </div>
-  );
+    // React Fragments
+    <>
+    <MyText />
+     <MyText />
+    <MyText />
+    </>
+
+    );
 }
