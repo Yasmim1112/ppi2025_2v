057b9673b3a6c7ba3e30a44efd6e97dfdb9b54cf
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-05-23 17:22:32 +0000

    Aula-23-05-25

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 
 export default function App() {
+const texts = [ {title: "Meu título ", text: "Meu texto "},
+  {title: "Meu título  ", text: "Meu texto "},
+  {title: "Meu título  ", text: "Meu texto "},
+  {title: "Meu título  ", text: "Meu texto "},
+  {title: "Meu título  ", text: "Meu texto "}
+];
+
   return (
     // React Fragments
     <>
-    <MyText />
-     <MyText />
-    <MyText />
-    </>
-
-    );
+    {texts.map((text, index) => {
+      return (
+        <MyText key={index} title={`${text.title} ${index + 1}`}>
+          {text.text}
+        </MyText>
+      );
+    })}</>
+  );
 }
--- src/components/MyText.jsx
+++ src/components/MyText.jsx
@@ -1,13 +1,12 @@
 import styles from './MyText.module.css';
 
-export function MyText(){
+export function MyText({ title, children}) {
     return (
         <div className={styles.container}>
         <div className={styles.div}>
-        <h1 className={styles.title}>Meu primeiro React</h1>
-        <p className= {styles.text}>
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-
+        <h1 className={styles.title}>{title}</h1>
+        <p className= {styles.text}>{children}
+   
 </p>
         </div>
         </div>
