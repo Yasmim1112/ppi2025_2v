db5553e913eda6b3531c5ec28f7c0ba3e68dc226
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-06-06 13:18:38 +0000

    Ajeitei_em_casa

--- src/components/MyGrid.jsx
+++ src/components/MyGrid.jsx
@@ -1,38 +1,37 @@
 import styles from './MyGrid.module.css';
-import { MyText } from './MyText';
 
-export function MyGrid(){
-return (
 
-    <main className={styles.main}>
+export function MyGrid() {
+  return (
+    <div className={styles.container}>
+      <header className={styles.header1} />
+      <header className={styles.header2} />
+      <aside className={styles.aside} />
+      <main className={styles.main}>
         <div className={styles.grid}>
-        <div className={styles.card}>
-            <h2>card1</h2>
-            <p>this is card</p>
+          <div className={styles.card}>
+            <h2>Card 1</h2>
+            <p>This is the first card.</p>
+          </div>
+          <div className={styles.card}>
+            <h2>Card 2</h2>
+            <p>This is the second card.</p>
+          </div>
+          <div className={styles.card}>
+            <h2>Card 3</h2>
+            <p>This is the third card.</p>
+          </div>
+          <div className={styles.card}>
+            <h2>Card 4</h2>
+            <p>This is the fourth card.</p>
+          </div>
+          <div className={styles.card}>
+            <h2>Card 5</h2>
+            <p>This is the fifth card.</p>
+          </div>
         </div>
-    <div className={styles.card}>
-            <h2>card1</h2>
-            <p>this is card</p>
-        </div>
-
-        <div className={styles.card}>
-            <h2>card2</h2>
-            <p>this is card</p>
-        </div>
-        <div className={styles.card}>
-            <h2>card3</h2>
-            <p>this is card</p>
-        </div>
-        <div className={styles.card}>
-            <h2>card4</h2>
-            <p>this is card</p>
-        </div>
-        <div className={styles.card}>
-            <h2>card5</h2>
-            <p>this is card</p>
-        </div>
-        </div>
-
-        </main>
-);
+      </main>
+      <footer className={styles.footer} />
+    </div>
+  );
 }
