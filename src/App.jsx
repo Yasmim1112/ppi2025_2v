443749f0c9b42f8797ba100e76eb5107ae99becf
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-06-12 17:26:28 +0000

    Prova-12-06

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,12 @@ import "./styles/global.css";
 import { MyText } from "./components/MyText";
 import { MyTextList } from "./components/MyTextList";
 import { Desafio} from "./components/Desafio";
+import { Prova } from "./components/Prova";
 
 export default function App() {
-
   return (
     <>
-      <Desafio/>
+      <Prova/>
     </>
   );
 }
