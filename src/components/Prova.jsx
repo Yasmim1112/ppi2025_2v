aec218c067dc04a82a2d03d0636e2f53757adb4d
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-06-13 14:15:19 +0000

    Ajeitei-em-casa

--- src/components/Prova.jsx
+++ src/components/Prova.jsx
@@ -1,5 +1,6 @@
 import styles from './Prova.module.css';
-import { BookHeadphones } from 'lucide-react';
+import { BookHeadphones, Github, Instagram, Phone  } from 'lucide-react';
+
 
 export function Prova() {
   return (
@@ -37,7 +38,27 @@ export function Prova() {
                    </div>
                  </div>
         </main>
-         <footer className={styles.footer} />
+         <footer className={styles.footer} >
+          <div className={styles.footerText}>
+          <h1>IFRN-Campus Macau</h1>
+          <h1>Curso Técnino em Informática</h1>
+           <h1>Programação para Internet 2025</h1>
+            </div>
+            <h2>Yasmim Fernandes dos Santos</h2>
+
+            <div className={styles.icons}>
+            <a href='https://github.com/Yasmim1112' target='_blank'>
+              <Github/>
+            </a>
+            <a href='https://instagram.com/yasmimfdossantos/' target='_blank'>
+              <Instagram/>
+            </a>
+            <a href='tel:+558497034815' target='_blank'>
+              <Phone/>
+            </a>
+          </div>
+         
+         </footer>
        </div>
      );
    }
