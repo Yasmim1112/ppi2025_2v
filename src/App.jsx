bbda18b91a1fdfbd9ed4c7a601cd8bf144040f5f
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-07-04 17:31:36 +0000

    aula

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { ProductList } from "./components/Etapa-2/ProductList";
+import { ProductList } from "./components/ProductList";
 
 export default function App() {
+
   return (
     // React Fragment
     <>
-     <ProductList />
+      <ProductList />
     </>
   );
 }
-
