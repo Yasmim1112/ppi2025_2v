51696e09b3703696cd254164b99dcc7d9c33db25
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-07-17 17:21:40 +0000

    a

--- src/components/Etapa-2/Product.jsx
+++ src/components/Etapa-2/Product.jsx
@@ -1,20 +1,40 @@
-import React from "react";
 import styles from "./Product.module.css";
+import { useState } from "react";
 
-export function Product({ thumbnail, title, description, price, onAddToCart }) {
+export function Product({ product, addToCart }) {
+  const [added, setAdded] = useState(false);
+  const [qty, setQty] = useState(0);
+
   return (
     <div className={styles.productCard}>
-      <img src={thumbnail} alt={title} className={styles.productImage} />
-      <h2 className={styles.productTitle}>{title}</h2>
-      <p className={styles.productDescription}>{description}</p>
-      <p className={styles.productPrice}>R$ {price.toFixed(2)}</p>
-      <button className={styles.addToCartButton} onClick={onAddToCart}>
-        Adicionar ao carrinho
+      <img
+        src={product.thumbnail}
+        alt={product.title}
+        className={styles.productImage}
+      />
+      <h2 className={styles.productTitle}>{product.title}</h2>
+      <p className={styles.productDescription}>{product.description}</p>
+      <div className={styles.productQty}>
+        <p className={styles.productPrice}>${product.price}</p>
+        {added && (
+          <div className={styles.productQty}>
+            <button>-</button>
+            <p>{qty}</p>
+            <button>+</button>
+          </div>
+        )}
+      </div>
+      <button
+        className={styles.productButton}
+        onClick={() => {
+          addToCart(product);
+          setAdded(true);
+          setQty(qty + 1);
+        }}
+      >
+        ADD TO CART
       </button>
     </div>
   );
 }
-
-
-
 
--- src/components/Etapa-2/ProductList.jsx
+++ src/components/Etapa-2/ProductList.jsx
@@ -3,32 +3,21 @@ import styles from "./ProductList.module.css";
 import { CircularProgress } from "@mui/material";
 import { Product } from "./Product";
 
- 
+export function ProductList({ addToCart }) {
+  var category = "beauty";
+  var limit = 12;
+  var apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
 
-export function ProductList() {
-  const category = "fragrances";
-  const limit = 10;
-  const apiUrl = `https://dummyjson.com/products/category/${category}?limit=${limit}&select=id,thumbnail,title,price,description`;
-
-  function handleClick(product) {
-    setCart((prevCart) => [...prevCart, product]);
-    console.log("Produto adicionado ao carrinho:", product);
-  }
-  
- const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [cart, setCart] = useState([]);
-
-  function getCartTotal(cart) {
-  return cart.reduce((total, product) => total + product.price, 0);
-}
 
   useEffect(() => {
     async function fetchProducts() {
       try {
         const response = await fetch(apiUrl);
         const data = await response.json();
+
         setProducts(data.products);
       } catch (error) {
         setError(error);
@@ -36,54 +25,33 @@ export function ProductList() {
         setLoading(false);
       }
     }
-
     setTimeout(() => {
       fetchProducts();
     }, 2000);
   }, []);
-
   return (
     <div className={styles.container}>
-      <h1>TRJ Megastore</h1>
-
+      <div className={styles.grid}>
+        {products.map((product) => (
+          <Product key={product.id} product={product} addToCart={addToCart} />
+        ))}
+      </div>
       {loading && (
         <div>
           <CircularProgress
+            // size="sm"
             thickness={5}
             style={{ margin: "2rem auto", display: "block" }}
-            sx={{ color: "#001111" }}
+            sx={{
+              color: "#001111",
+            }}
           />
           <p>Loading products...</p>
         </div>
       )}
-
       {error && <p>Error loading products: {error.message}</p>}
-
-      <div className={styles.productsGrid}>
-        {products
-          .filter(product => product && product.thumbnail && product.title && product.price)
-          .map(product => (
-            <Product
-              key={product.id}
-              thumbnail={product.thumbnail}
-              title={product.title}
-              description={product.description}
-              price={product.price}
-               onAddToCart={() => handleClick(product)} 
-            />
-          ))}
-      </div>
-      <div>
-        <h2>Valor total:</h2>
-       <p>R$ {getCartTotal(cart).toFixed(2)}</p>
-      </div>
     </div>
   );
 }
-
-
-
-
-
 
 
