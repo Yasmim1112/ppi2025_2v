3cb626d8f91de0b0919e5572ecef9db670a85fa7
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-07-18 17:21:56 +0000

    Aula-18/07

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { ProductList } from "./components/ProductList";
-import { Header } from "./components/Header";
+import { ProductList } from "./components/Etapa-2/ProductList";
+import { Header } from "./components/Etapa-1/Header";
 import { useState } from "react";
+import { Cart } from "./components/Etapa-2/Cart";
+import { Route, Routes } from "react-router";
 
 export default function App() {
-  
   const [cart, setCart] = useState([]);
+
   function addToCart(product) {
     setCart((prevCart) => [...prevCart, product]);
   }
+
   return (
     // React Fragment
     <>
       <Header cart={cart} />
-      <ProductList addToCart={addToCart} />
+      <Routes>
+        <Route path="/" element={<ProductList addToCart={addToCart} />} />
+        <Route path="/cart" element={<Cart cart={cart} />} />
+        {/* Add more routes as needed */}
+      </Routes>
     </>
   );
 }
