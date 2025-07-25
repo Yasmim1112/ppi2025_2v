705ce1d7402d915b475d517f1512b2750dfcd1fc
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-07-25 16:53:12 +0000

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import "./styles/theme.css";
 import "./styles/global.css";
-import { Header} from "./Etapa-1/components/Header";
-import { ProductList } from "./Etapa-2/components/ProductList";
-import { Cart } from "./Etapa-2/components/Cart";
+import { Header} from "./components/Etapa-1/Header";
+import { ProductList } from "./components/Etapa-2/ProductList";
+import { Cart } from "./components/Etapa-2/Cart";
 import { Route, Routes } from "react-router";
-import { CartProvider } from "./service/CartContext";
+import { CartProvider } from "./components/service/CartContext";
 
 export default function App() {
   return (
