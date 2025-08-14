ffb54f238ceafd402b33c3626752bd0b9343c92b
Yasmim1112 <yasmimfdossantos@gmail.com>
2025-08-14 20:44:43 +0000

    metade

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { Cart } from "./components/Etapa-2/Cart";
 import { Route, Routes } from "react-router";
 import { CartProvider } from "./components/service/CartContext";
 import { TelaDel } from "./components/Etapa-2/Tela-del";
+import { TelaDer } from "./components/Etapa-2/Tela-der";
 
 export default function App() {
   return (
@@ -17,6 +18,7 @@ export default function App() {
           <Route path="/" element={<ProductList />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<TelaDel />} />
+          <Route path="/tela-der" element={<TelaDer />} />
           {/* Add more routes as needed */}
         </Routes>
       </CartProvider>
--- src/components/Etapa-2/Tela-der.jsx
+++ src/components/Etapa-2/Tela-der.jsx
@@ -1,21 +1,20 @@
-import styles from "./Tela-del.module.css";
+import styles from "./Tela-der.module.css";
 import { Link } from "react-router";
 
 
-export function TelaDel() { 
+export function TelaDer() { 
     function handleLogin() {
       <Link to="/login"><CircleUserRound /></Link>
     }
     return (
         <div className={styles.container}>
-        <h1>Bem-vindo(a) a TRJ Megastore!</h1>
+            <h1>Tela de cadastro</h1> 
         <form className={styles.form}>
             <label htmlFor="email">Email:</label>
             <input type="email" id="email" name="email" required />
             <label htmlFor="password">Senha:</label>
             <input type="password" id="password" name="password" required />
-            <button type= "submit" onClick={handleLogin}>Entrar</button>
-            <Link to="/tela-der"><button type="button">Registrar</button></Link>
+            <button type="button">Registrar</button>
         </form>
         </div>
     );
